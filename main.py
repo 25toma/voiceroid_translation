eec15035bb1b2abedc0efaab3c862e847bc050d5
25toma <2525toa@gmail.com>
2018-09-08 12:10:06 +0900

    voiro_autoを抽象,継承クラス化

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import translate
 import config
 
 #----------実行----------
-transObj = translate.TranslateNormal()
+trans_obj = translate.TranslateNormal()
+voiro_obj = voiro_auto.Voiro_Pywinauto()
 
 while True:
     #英語を録音して、日本語にする
     raw_texts = recording.record()
     if raw_texts is None:
         continue
-    trans_texts = transObj.trans(raw_texts)
+    trans_texts = trans_obj.trans(raw_texts)
     trans_texts = "\r\n".join(trans_texts)
     
     #ボイロの子に喋ってもらう
-    voiro_auto.talkVOICEROID2(trans_texts)
+    voiro_obj.talk(trans_texts)
     
     #デバッグ出力
     if config.TEXT_CHECK:
--- voiro_auto.py
+++ voiro_auto.py
@@ -1,95 +1,102 @@
 # -*- coding: utf-8 -*-
 import pywinauto
+from abc import ABCMeta, abstractmethod
 
-def search_child_byclassname(class_name, uiaElementInfo, target_all = False):
-    target = []
-    # 全ての子要素検索
-    for childElement in uiaElementInfo.children():
-        # ClassNameの一致確認
-        if childElement.class_name == class_name:
-            if target_all == False:
+class VoiroAuto_Meta(metaclass=ABCMeta):
+    @abstractmethod
+    def talk(self, text):
+        raise NotImplemented()
+
+class Voiro_Pywinauto(VoiroAuto_Meta):
+    def search_child_byclassname(self, class_name, uiaElementInfo, target_all = False):
+        target = []
+        # 全ての子要素検索
+        for childElement in uiaElementInfo.children():
+            # ClassNameの一致確認
+            if childElement.class_name == class_name:
+                if target_all == False:
+                    return childElement
+                else:
+                    target.append(childElement)
+        if target_all == False:
+            # 無かったらFalse
+            return False
+        else:
+            return target
+
+
+    def search_child_byname(self, name, uiaElementInfo):
+        # 全ての子要素検索
+        for childElement in uiaElementInfo.children():
+            # Nameの一致確認
+            if childElement.name == name:
                 return childElement
-            else:
-                target.append(childElement)
-    if target_all == False:
         # 無かったらFalse
         return False
-    else:
+
+    def talk(self, speakPhrase):
+        # デスクトップのエレメント
+        parentUIAElement = pywinauto.uia_element_info.UIAElementInfo()
+        # voiceroidを捜索する
+        voiceroid2 = self.search_child_byname("VOICEROID2",parentUIAElement)
+        # *がついている場合
+        if voiceroid2 == False:
+            voiceroid2 = self.search_child_byname("VOICEROID2*",parentUIAElement)
+
+        # テキスト要素のElementInfoを取得
+        TextEditViewEle = self.search_child_byclassname("TextEditView",voiceroid2)
+        textBoxEle      = self.search_child_byclassname("TextBox",TextEditViewEle)
+
+        # コントロール取得
+        textBoxEditControl = pywinauto.controls.uia_controls.EditWrapper(textBoxEle)
+
+        # テキスト登録
+        textBoxEditControl.set_edit_text(speakPhrase)
+
+
+        # ボタン取得
+        buttonsEle = self.search_child_byclassname("Button",TextEditViewEle,target_all = True)
+        # 再生ボタンを探す
+        playButtonEle = ""
+        for buttonEle in buttonsEle:
+            # テキストブロックを捜索
+            textBlockEle = self.search_child_byclassname("TextBlock",buttonEle)
+            if textBlockEle.name == "再生":
+                playButtonEle = buttonEle
+                break
+
+        # ボタンコントロール取得
+        playButtonControl = pywinauto.controls.uia_controls.ButtonWrapper(playButtonEle)
+
+        # 再生ボタン押下
+        playButtonControl.click()
+
+
+
+    #----- In development functions ------------------
+    def look_children(self, obj):
+        for child in obj.children():
+            print(child.class_name)
+            #print(child.handle(),end=" ")
+            #print(child.automation_id())
+
+
+    def get_voiro2(self):
+        # デスクトップのエレメント
+        parentUIAElement = pywinauto.uia_element_info.UIAElementInfo()
+        # voiceroidを捜索する
+        voiceroid2 = self.search_child_byname("VOICEROID2",parentUIAElement)
+        # *がついている場合
+        if voiceroid2 == False:
+            voiceroid2 = self.search_child_byname("VOICEROID2*",parentUIAElement)
+        return voiceroid2
+
+    def get_tuning_tab(self, tabName, voiro2=None):
+        if voiro2==None:
+            voiro2 = self.get_voiro2()
+        tuning_tab = self.search_child_byclassname("TabControl", voiro2, target_all=True)[1]
+        target = self.search_child_byname(tabName, tuning_tab)
         return target
-
-
-def search_child_byname(name, uiaElementInfo):
-    # 全ての子要素検索
-    for childElement in uiaElementInfo.children():
-        # Nameの一致確認
-        if childElement.name == name:
-            return childElement
-    # 無かったらFalse
-    return False
-
-def talkVOICEROID2(speakPhrase):
-    # デスクトップのエレメント
-    parentUIAElement = pywinauto.uia_element_info.UIAElementInfo()
-    # voiceroidを捜索する
-    voiceroid2 = search_child_byname("VOICEROID2",parentUIAElement)
-    # *がついている場合
-    if voiceroid2 == False:
-        voiceroid2 = search_child_byname("VOICEROID2*",parentUIAElement)
-
-    # テキスト要素のElementInfoを取得
-    TextEditViewEle = search_child_byclassname("TextEditView",voiceroid2)
-    textBoxEle      = search_child_byclassname("TextBox",TextEditViewEle)
-
-    # コントロール取得
-    textBoxEditControl = pywinauto.controls.uia_controls.EditWrapper(textBoxEle)
-
-    # テキスト登録
-    textBoxEditControl.set_edit_text(speakPhrase)
-
-
-    # ボタン取得
-    buttonsEle = search_child_byclassname("Button",TextEditViewEle,target_all = True)
-    # 再生ボタンを探す
-    playButtonEle = ""
-    for buttonEle in buttonsEle:
-        # テキストブロックを捜索
-        textBlockEle = search_child_byclassname("TextBlock",buttonEle)
-        if textBlockEle.name == "再生":
-            playButtonEle = buttonEle
-            break
-
-    # ボタンコントロール取得
-    playButtonControl = pywinauto.controls.uia_controls.ButtonWrapper(playButtonEle)
-
-    # 再生ボタン押下
-    playButtonControl.click()
-
-
-
-#----- In development functions ------------------
-def look_children(obj):
-    for child in obj.children():
-        print(child.class_name)
-        #print(child.handle(),end=" ")
-        #print(child.automation_id())
-
-
-def get_voiro2():
-    # デスクトップのエレメント
-    parentUIAElement = pywinauto.uia_element_info.UIAElementInfo()
-    # voiceroidを捜索する
-    voiceroid2 = search_child_byname("VOICEROID2",parentUIAElement)
-    # *がついている場合
-    if voiceroid2 == False:
-        voiceroid2 = search_child_byname("VOICEROID2*",parentUIAElement)
-    return voiceroid2
-
-def get_tuning_tab(tabName, voiro2=None):
-    if voiro2==None:
-        voiro2 = get_voiro2()
-    tuning_tab = search_child_byclassname("TabControl", voiro2, target_all=True)[1]
-    target = search_child_byname(tabName, tuning_tab)
-    return target
 
 if __name__=="__main__":
     pass
