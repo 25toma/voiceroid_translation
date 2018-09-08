56b8c43971381d698176fc10ba1e7010beeb6b41
25toma <2525toa@gmail.com>
2018-09-08 21:54:48 +0900

    Recruit社のTalk APIを使えるモジュールを、translateに追加

--- translate.py
+++ translate.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-from googletrans import Translator
-from google.cloud import translate
 from abc import ABCMeta, abstractmethod
-import six
 import config
 
 #-----抽象クラス-----
@@ -18,6 +15,7 @@ class TranslateMeta(metaclass=ABCMeta):
 #通常のgoogle翻訳を使用(無料)
 class TranslateNormal(TranslateMeta):
     def __init__(self):
+        from googletrans import Translator
         self.translator = Translator()
     
     def trans(self, raw_texts):
@@ -31,9 +29,11 @@ class TranslateNormal(TranslateMeta):
 #googleの NeuralNet APIを使用(有料)
 class TranslateNMT(TranslateMeta):
     def __init__(self):
+        from google.cloud import translate
         self.translate_client = translate.Client()
     
     def trans(self, raw_texts):
+        import six
         for i in range(len(raw_texts)):
             if isinstance(raw_texts[i], six.binary_type):
                 raw_texts[i]=raw_texts[i].decode("utf-8")
@@ -42,6 +42,60 @@ class TranslateNMT(TranslateMeta):
                     for raw_text in raw_texts]
         trans_texts = [result["translatedText"] for result in results]
         return trans_texts
+
+#Recruitの A3RTを使用
+class Communicate_A3RT(TranslateMeta):
+    def __init__(self):
+        self.URL = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk"
+        self.okCode = 200
+
+    def trans(self, raw_texts):
+        import json
+        import requests
+        while type(raw_texts) is list:
+            raw_texts = "。".join(raw_texts)
+        params = {
+            "apikey" : (None, config.A3RTkey),
+            "query" : (None, raw_texts),
+        }
+        response = requests.post(self.URL, files = params)
+        parsed = json.loads(response.text)
+        if response.status_code == self.okCode:
+            text = []
+            for colume in parsed["results"]:
+                text += [colume["reply"]]
+            return text
+        else:
+            return parsed["message"]
+
+    def response_analyze(self,response):
+        print(dir(response))
+        print("-"*10)
+        print(response.connection)
+        print("-"*10)
+        print(response.content)
+        print("-"*10)
+        print(response.encoding)
+        print("-"*10)
+        print(response.headers)
+        print("-"*10)
+        print(response.history)
+        print("-"*10)
+        print(response.json)
+        print("-"*10)
+        print(response.ok)
+        print("-"*10)
+        print(response.raw)
+        print("-"*10)
+        print(response.reason)
+        print("-"*10)
+        print(response.request)
+        print("-"*10)
+        print(response.status_code)
+        print("-"*10)
+        print(response.text)
+        print("-"*10)
+        print(response.url)
 
 #デバッグ用
 if __name__ == "__main__":
